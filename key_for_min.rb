0ffd5035213643ef17aeae25230e0421ed24f671
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-08 12:53:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
-  name_hash.each do |k, v|
+  name_hash.each do |key, v|
     if lowest_value == nil || v < lowest_value
       lowest_value = v 
       lowest_key = k 
